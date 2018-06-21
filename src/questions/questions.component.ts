9d161468f384d889c67323a19dd1038bc4aac607
kyller92 <kyller92@hotmail.fr>
2018-06-21 10:38:05 +0200

    Added template, Updated QuestionsComponent

--- src/questions/questions.component.ts
+++ src/questions/questions.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { Question } from './models'
+import { NavController, NavParams } from 'ionic-angular';
 import { QuestionsService } from './service'
 
 @Component({
@@ -8,5 +9,31 @@ import { QuestionsService } from './service'
 })
 
 export class Questions {
-	
+	public questions: Question[];
+	public question: Question;
+	page: number = 1;
+
+	constructor(public navController: NavController, navParams: NavParams, public questionsServices: QuestionsService) {
+		this.question = navParams.get('question');
+	}
+
+	ionViewWillEnter() {
+		this.questionsServices.loadQuestions();
+		this.questionsServices
+			.subject
+			.asObservable()
+			.subscribe(messages => this.questions = this.questions)
+	}
+
+	refresh(event) {
+		this
+			.questionsServices
+			.loadQuestions()
+			.then(response => {
+				event.complete();
+			})
+			.catch(error =>{
+				event.complete();
+			})
+	}
 }
